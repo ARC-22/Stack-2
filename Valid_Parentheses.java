c70e93727dee3ea5051668ffd5746e4dd6c13250
Advait Churi <advaitchuri@Advaits-MacBook-Pro.local>
2024-07-29 19:22:14 -0700

    Stack-2

---
+++ Valid_Parentheses.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                st.push(')');
+            }
+            else if(c == '{'){
+                st.push('}');
+            }
+            else if(c == '['){
+                st.push(']');
+            }
+            else if(st.isEmpty() || (st.peek() != c)){
+                return false;
+            }
+            else{
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
